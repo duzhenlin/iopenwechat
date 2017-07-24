3992e94534b150f05fe2464b3df51a61998139e9
Duzhenlin <duzhenlin@vip.qq.com>
2017-07-24 23:49:32 +0800

    模板消息

---
+++ src/Core/ServiceProviders/NoticeServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: duzhenlin
+ * Date: 2017/7/24
+ * Time: 23:15
+ */
+
+namespace IopenWechat\Core\ServiceProviders;
+
+
+class NoticeServiceProvider
+{
+
+}
---
+++ src/Notice/Notice.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: duzhenlin
+ * Date: 2017/7/24
+ * Time: 23:17
+ */
+
+namespace IopenWechat\Notice;
+
+
+class Notice
+{
+
+}
